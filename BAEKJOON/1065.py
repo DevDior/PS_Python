6c36d280bc5adf3a8a2dc3a5bb8143b81cf94044
Hyeokgi <chinup1004@naver.com>
2021-08-21 19:56:36 +0900

    function problem

---
+++ BAEKJOON/1065.py
@@ -0,0 +1,18 @@
+# 한수
+
+import sys
+
+def sol(N):
+    oneNum = 0;
+    if N < 100:
+        oneNum = N
+    else:
+        oneNum = 99
+        for i in range(100, N+1):
+            if i//100 - (i%100)//10 == (i%100)//10 - ((i%100)%10):
+                oneNum += 1
+
+    return oneNum
+
+N = int(sys.stdin.readline())
+print(sol(N))
