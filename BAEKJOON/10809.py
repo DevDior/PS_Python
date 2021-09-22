22ed789a207665121551457b5db4e8dff01bf7e8
Hyeokgi <chinup1004@naver.com>
2021-09-22 18:30:15 +0900

    String problem

---
+++ BAEKJOON/10809.py
@@ -0,0 +1,13 @@
+# 알파벳 찾기
+
+import sys
+
+word = sys.stdin.readline()
+abc_list = [-1 for i in range(26)]
+
+for i in range(len(word)-1):
+    if(abc_list[ord(word[i])-97] == -1):
+        abc_list[ord(word[i])-97] = i
+
+for j in abc_list:
+    print(j, end=" ")
---
+++ BAEKJOON/11720.py
@@ -0,0 +1,12 @@
+# 숫자의 합
+
+import sys
+
+answer = 0
+T = int(sys.stdin.readline())
+n = sys.stdin.readline()
+
+for i in range(T):
+    answer += int(n[i])
+
+print(answer)
