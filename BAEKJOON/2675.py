df8d08aea94a6afe3e4aa535a91c882acc972335
Hyeokgi <chinup1004@naver.com>
2021-09-25 20:57:19 +0900

    solved.ac Class 1+

---
+++ BAEKJOON/2675.py
@@ -0,0 +1,12 @@
+# 문자열 반복
+
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    R, S = sys.stdin.readline().split()
+    R = int(R)
+    for i in range(len(S)):
+        print(S[i]*R, end='')
+    print()
---
+++ BAEKJOON/2920.py
@@ -0,0 +1,27 @@
+# 음계
+
+import sys
+
+nums = sys.stdin.readline().split()
+numLen = len(nums)
+mixed = False
+
+for i in range(numLen):
+    nums[i] = int(nums[i])
+
+if(nums[0] ==1):
+    for _ in range(1, numLen):
+        if(nums[_] - nums[_-1] != 1):
+            print("mixed")
+            mixed = True
+            break
+    if(mixed ==False) : print("ascending")
+elif(nums[0] == 8):
+    for _ in range(1, numLen):
+        if(nums[_] - nums[_-1] != -1):
+            print("mixed")
+            mixed = True
+            break
+    if(mixed == False): print("descending")
+else:
+    print("mixed")
