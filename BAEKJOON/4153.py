1f6a75093e347c395bffcfcb3afe6a569663e974
Hyeokgi <chinup1004@naver.com>
2021-09-27 00:02:40 +0900

    BOJ Class2

---
+++ BAEKJOON/4153.py
@@ -0,0 +1,12 @@
+# 직삼각형
+import sys
+
+while(True):
+    nums = list(map(int, sys.stdin.readline().split()))
+    if(nums == [0, 0, 0]): break
+
+    maxNum = max(nums)
+    nums.remove(maxNum)
+
+    if(maxNum**2 == nums[0]**2 + nums[1]**2): print("right")
+    else: print("wrong")
