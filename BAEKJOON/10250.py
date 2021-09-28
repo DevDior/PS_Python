988e281930cc0504732fb8ecdd330cc1268f6b74
Hyeokgi <chinup1004@naver.com>
2021-09-28 13:46:21 +0900

    Clear Class 1

---
+++ BAEKJOON/10250.py
@@ -0,0 +1,8 @@
+# ACM 호텔 can't solve
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+board = []
+for _ in range(N):
+    board.append(sys.stdin.readline())
+
--- BAEKJOON/1157.py
+++ BAEKJOON/1157.py
@@ -1,1 +1,13 @@
 # 단어 공부 can't solve
+# solve: v
+import sys
+word = sys.stdin.readline().upper().rstrip()
+
+countAlpabat = list(set(word))
+countNums = []
+
+for i in range(len(countAlpabat)):
+    countNums.append(word.count(countAlpabat[i]))
+
+if(countNums.count(max(countNums)) > 1): print("?")
+else: print(countAlpabat[countNums.index(max(countNums))])
