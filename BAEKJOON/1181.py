a19049521c0cad9b6900ed15768a8b4c3a47e2f6
Hyeokgi <chinup1004@naver.com>
2021-10-13 03:02:32 +0900

    1181.py

---
+++ BAEKJOON/1181.py
@@ -0,0 +1,18 @@
+# 단어 정렬
+
+import sys
+
+wordList = []
+
+N = int(sys.stdin.readline())
+for i in range(N):
+    word = sys.stdin.readline().strip()
+    if(word in wordList):
+        pass
+    else:
+        wordList.append(word)
+
+wordList.sort(key = lambda x: (len(x), x))
+
+for j in wordList:
+    print(j)
