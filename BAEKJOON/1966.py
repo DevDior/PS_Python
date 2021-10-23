eba0cf7fecbb869db7fad0ab0c4317c69c0e4970
Hyeokgi <chinup1004@naver.com>
2021-10-23 15:45:47 +0900

    1966

---
+++ BAEKJOON/1966.py
@@ -0,0 +1,23 @@
+# 프린터 큐
+
+import sys
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    answer = 0
+    n, w = map(int, sys.stdin.readline().split())
+    q = list(map(int, sys.stdin.readline().split()))
+    while(1):
+        if max(q) == q[0]:
+            if w == 0:
+                answer += 1
+                break
+            q.pop(0)
+            if w > 0: w -= 1
+            answer += 1
+        else:
+            q.append(q.pop(0))
+            w -= 1
+            if w < 0: w = len(q) - 1
+    print(answer)
