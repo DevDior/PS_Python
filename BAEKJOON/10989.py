ec27d39364c50db35842ed12206b9e41234f9644
Hyeokgi <chinup1004@naver.com>
2021-11-12 18:50:37 +0900

    repeat problem

--- BAEKJOON/10989.py
+++ BAEKJOON/10989.py
@@ -1,9 +1,16 @@
 # 수 정렬하기 3
+# repeat: v
 
 import sys
 
 N = int(sys.stdin.readline())
 
-answerList = [0] *5
+answerList = [0]*10001
 
-print(answerList)
+for i in range(N):
+    answerList[int(sys.stdin.readline())] += 1
+
+for j in range(1, 10001):
+    if answerList[j] != 0:
+        for _ in range(answerList[j]):
+            print(j)
