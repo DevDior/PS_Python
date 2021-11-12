9d32d62badf8e060d166c4d432239737afae8efe
Hyeokgi <chinup1004@naver.com>
2021-11-12 18:46:26 +0900

    BOJ Class2

---
+++ BAEKJOON/10989.py
@@ -0,0 +1,9 @@
+# 수 정렬하기 3
+
+import sys
+
+N = int(sys.stdin.readline())
+
+answerList = [0] *5
+
+print(answerList)
---
+++ BAEKJOON/11650.py
@@ -0,0 +1,15 @@
+# 좌표 정렬하기
+
+import sys
+
+N = int(sys.stdin.readline())
+answerList = []
+
+for i in range(N):
+    answerList.append(list(map(int, sys.stdin.readline().split())))
+
+answerList.sort()
+
+for j in answerList:
+    print(j[0], end=' ')
+    print(j[1])
