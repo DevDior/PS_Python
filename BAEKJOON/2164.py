89968ede38fc68f0c2421aa2210b2367b17bfc47
Hyeokgi <chinup1004@naver.com>
2022-01-04 03:07:14 +0900

    silver4

---
+++ BAEKJOON/2164.py
@@ -0,0 +1,15 @@
+#카드2
+
+import sys
+
+N = int(sys.stdin.readline())
+card = [x for x in range(1, N+1)]
+
+while len(card) != 1:
+    if len(card)%2 == 0:
+        card = [card[x] for x in range(1, len(card), 2)]
+    else:
+        card = [card[x] for x in range(1, len(card), 2)]
+        card.append(card.pop(0))
+    
+print(card[0])
