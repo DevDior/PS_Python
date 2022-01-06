daa349762f94a29d9c17947a2c17f4428ab61566
Hyeokgi <chinup1004@naver.com>
2022-01-07 01:54:20 +0900

    celar

---
+++ BAEKJOON/2798.py
@@ -0,0 +1,16 @@
+# 블랙잭
+
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+cardList = list(map(int, sys.stdin.readline().split()))
+
+answer = 0
+
+for i in cardList:
+    for j in cardList[cardList.index(i)+1:]:
+        for k in cardList[cardList.index(j)+1:]:
+            if i+j+k > answer and i+j+k <= M:
+                answer = i+j+k
+
+print(answer)
