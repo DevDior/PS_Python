b9d4cad75443a09117e35f40b04faddcad49f1d3
Hyeokgi <chinup1004@naver.com>
2022-01-08 02:57:14 +0900

    clear

---
+++ BAEKJOON/1259.py
@@ -0,0 +1,35 @@
+# 팰린드롬수
+
+import sys
+
+while True:
+    answer = True
+    s = 0
+    e = -1
+    
+    pld = sys.stdin.readline().strip()
+    
+    if pld == '0':
+        break
+    
+    if len(pld) %2 == 0:
+        while s < len(pld)//2:
+            if pld[s] != pld[e]:
+                answer = False
+                break
+            s += 1
+            e -= 1
+            
+    else:
+        while s < len(pld)//2:
+            if pld[s] != pld[e]:
+                answer = False
+                break
+            s += 1
+            e -= 1
+
+    if answer == True:
+        print('yes')
+    else:
+        print('no')
+        
