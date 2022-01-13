97467542d1d74dbff7c986c0dda049d603d928cb
Hyeokgi <chinup1004@naver.com>
2022-01-14 01:09:16 +0900

    repeat

--- BAEKJOON/7568.py
+++ BAEKJOON/7568.py
@@ -4,32 +4,14 @@ import sys
 
 N = int(sys.stdin.readline())
 group = []
-answer = [0 for x in range(N)]
 
 for _ in range(N):
     group.append(tuple(map(int, sys.stdin.readline().split())))
-
-rank = 1
-count = 1
-
-while max(group)[1] != 0:
-    inform = max(group)
-    index = group.index(inform)
-    group[index] = (0, 0)
-    answer[index] = rank
-    count += 1
+    
+for i in range(N):
+    rank = 1
     
     for j in range(N):
-        if inform[1] < group[j][1] or inform == group[j]:
-            index2 = group.index(group[j])
-            group[j] = (0, 0)
-            answer[index2] = rank
-            count += 1
-    
-    rank = count
-    
-for j in range(len(answer)):
-    if j != len(answer)-1:
-        print(answer[j], end=' ')
-    else:
-        print(answer[j])
+        if group[i][0] < group[j][0] and group[i][1] < group[j][1]:
+            rank += 1
+    print(rank, end=' ')
