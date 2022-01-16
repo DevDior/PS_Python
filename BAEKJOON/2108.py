7cd64ad9516322cee8e8bfaeaf60d35db174b4be
Hyeokgi <chinup1004@naver.com>
2022-01-16 23:00:02 +0900

    repeat

---
+++ BAEKJOON/2108.py
@@ -0,0 +1,37 @@
+# 통계학
+
+import sys
+import collections
+
+N = int(sys.stdin.readline())
+numList = []
+
+for _ in range(N):
+    numList.append(int(sys.stdin.readline()))
+numList.sort()
+
+count = collections.Counter(numList)
+maxDuplicateNumList = count.most_common()
+maxDuplicateNumList.sort(reverse=True, key=lambda x : x[1])
+
+if len(maxDuplicateNumList) == 1:
+    maxDuplicateNum = maxDuplicateNumList[0][0]
+else:
+    if maxDuplicateNumList[0][1] == maxDuplicateNumList[1][1]:
+        maxDuplicateNum = maxDuplicateNumList[1][0]
+    else:
+        maxDuplicateNum = maxDuplicateNumList[0][0]
+          
+sumNum = sum(numList)
+if ((sumNum/N)*10) % 10 >= 5:
+    print(sumNum//N+1)
+else:
+    print(sumNum//N)
+    
+print(numList[N//2])
+print(maxDuplicateNum)
+
+if N == 1:
+    print(0)
+else:
+    print(max(numList)-min(numList))
