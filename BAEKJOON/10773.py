fc065d5526bf4baa35cbea3d272c5510ed3d8f79
Hyeokgi <chinup1004@naver.com>
2022-01-18 01:06:32 +0900

    clear

---
+++ BAEKJOON/10773.py
@@ -0,0 +1,14 @@
+# 제로
+
+import sys
+K = int(sys.stdin.readline())
+
+answer = []
+
+for i in range(K):
+    answer.append(int(sys.stdin.readline()))
+    if answer[-1] == 0:
+        answer.pop(-1)
+        answer.pop(-1)
+
+print(sum(answer))
