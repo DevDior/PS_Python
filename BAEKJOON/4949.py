f72dc02408dd7072de7bfe828902a9255821b2fa
Hyeokgi <chinup1004@naver.com>
2022-01-19 02:06:43 +0900

    clear

---
+++ BAEKJOON/4949.py
@@ -0,0 +1,29 @@
+# 균형잡힌 세상
+
+import sys
+
+while True:
+    string = sys.stdin.readline().rstrip()
+    compareList = []
+    answer = True
+    
+    for i in string:
+        if i == '(' or i == '[':
+            compareList.append(i)
+        elif i == ')':
+            if len(compareList) == 0 or compareList.pop(-1) != '(':
+                print('no')
+                answer = False
+                break
+        elif i == ']':
+            if len(compareList) == 0 or compareList.pop(-1) != '[':
+                print('no')
+                answer = False
+                break
+            
+    if len(string) == 1 and string == '.':
+        break
+    elif len(compareList) != 0 and answer == True:
+        print('no')
+    elif answer == True:
+        print('yes')
