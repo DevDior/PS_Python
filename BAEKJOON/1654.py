e23c9c960d57650546dcdb31489c295260347479
Hyeokgi <chinup1004@naver.com>
2022-01-25 23:14:57 +0900

    repeat

--- BAEKJOON/1654.py
+++ BAEKJOON/1654.py
@@ -1,4 +1,4 @@
-# 랜선 자르기
+# 랜선 자르기 v
 
 import sys
 
@@ -7,18 +7,35 @@ lanList = []
 
 for _ in range(K):
     lanList.append(int(sys.stdin.readline()))
-    
-maxLan = max(lanList)
-answer = 0
+#    
+#maxLan = max(lanList)
+#answer = 0
+#
+#for i in range(maxLan, 0, -1):
+#    count = 0
+#    
+#    for j in lanList:
+#        count += j//i
+#    
+#    if count >= N: 
+#        answer = i
+#        break
+#        
+#print(answer)
 
-for i in range(maxLan, 0, -1):
+start = 1
+end = max(lanList)
+
+while start <= end:
     count = 0
+    mid = (start + end)//2
     
-    for j in lanList:
-        count += j//i
+    for i in lanList:
+        count += i//mid
     
     if count >= N:
-        answer = i
-        break
-        
-print(answer)
+        start = mid + 1
+    else:
+        end = mid - 1
+
+print(end)
