62e8697dd1a792e07e398e4fa295c7af180e48ea
Hyeokgi Lee <chinup1004@koreatech.ac.kr>
2022-11-08 00:15:35 +0900

    V(BFS), Should solve by using DFS

---
+++ BAEKJOON/1012.py
@@ -0,0 +1,42 @@
+# 유기농 배추
+import sys
+from collections import deque
+
+T = int(sys.stdin.readline())
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+for _ in range(T):
+    M, N, K = map(int, sys.stdin.readline().split())
+    cabbage = [ list(map(int, sys.stdin.readline().split())) for _ in range(K)]
+    earthworm_count = 0
+    
+########## BFS ##########
+    queue = deque()
+    
+    while cabbage:
+        earthworm_count += 1
+        queue.append(cabbage.pop(0))
+        
+        while queue:
+            target = queue.popleft()
+            tx, ty = target[0], target[1]
+            
+            for i in range(4):
+                x = tx + dx[i]
+                y = ty + dy[i]
+                
+                if x < 0 or x >= M or y < 0 or y >= N:
+                    continue
+            
+                else:
+                    if [x, y] in cabbage:
+                        queue.append([x, y])
+                        cabbage.remove([x, y])
+                        
+                    else:
+                        continue
+        
+    print(earthworm_count)
+
+# DFS
