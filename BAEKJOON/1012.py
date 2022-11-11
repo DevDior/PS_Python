163d6c7b477cde6e03be4b9e6781a05ec5c5a548
Hyeokgi Lee <chinup1004@koreatech.ac.kr>
2022-11-12 05:13:01 +0900

    V, V(Solve by using DFS-recurrence)

--- BAEKJOON/1012.py
+++ BAEKJOON/1012.py
@@ -1,10 +1,29 @@
 # 유기농 배추
 import sys
-from collections import deque
-
+#from collections import deque
+sys.setrecursionlimit(10**6)
+    
 T = int(sys.stdin.readline())
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
+
+def DFS(target):
+    tx, ty = target[0], target[1]
+        
+    for i in range(4):
+        x = tx + dx[i]
+        y = ty + dy[i]
+        
+        if x < 0 or x >= M or y < 0 or y >= N:
+            continue
+        
+        else:
+            if [x, y] in cabbage:
+                cabbage.remove([x, y])
+                DFS([x, y])
+                
+            else:
+                continue
 
 for _ in range(T):
     M, N, K = map(int, sys.stdin.readline().split())
@@ -39,30 +58,14 @@ for _ in range(T):
     #    
     #print(earthworm_count)
 
-########## DFS ##########
-    stack= []
-    
+########## DFS - 재귀 ##########
     while cabbage:
         earthworm_count += 1
-        stack.append(cabbage.pop(0))
+        target = cabbage.pop(0)
         
-        while stack:
-            target = stack.pop(-1)
-            tx, ty = target[0], target[1]
-            
-            for i in range(4):
-                x = tx + dx[i]
-                y = ty + dy[i]
-                
-                if x < 0 or x >= M or y < 0 or y >= N:
-                    continue
-                
-                else:
-                    if [x, y] in cabbage:
-                        stack.append([x, y])
-                        cabbage.remove([x, y])
-                        
-                    else:
-                        continue
+        DFS(target)
                     
     print(earthworm_count)
+    
+    
+########## DFS - Stack ##########
