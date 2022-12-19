8fba7d34ee234b0b4556cbe0abdb2d5db107c527
Hyeokgi <71318080+JuniorHyeokgi@users.noreply.github.com>
2022-12-19 13:03:46 +0900

    Clear

---
+++ BAEKJOON/11047.py
@@ -0,0 +1,16 @@
+# 동전0
+import sys
+
+answer = 0
+N, K = map(int, sys.stdin.readline().split())
+coin_list = [int(sys.stdin.readline()) for x in range(N)]
+
+while K != 0:
+    if K >= coin_list[-1]:
+        K -= coin_list[-1]
+        answer += 1
+    
+    else:
+        coin_list.pop(-1)
+    
+print(answer)
