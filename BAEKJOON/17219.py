12ceb5e22a228e80c968749ab7d570be330b75be
Hyeokgi <71318080+JuniorHyeokgi@users.noreply.github.com>
2022-12-20 10:30:11 +0900

    Clear

---
+++ BAEKJOON/17219.py
@@ -0,0 +1,13 @@
+# 비밀번호 찾기
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+dic_account = dict()
+
+for i in range(N):
+    id, password = sys.stdin.readline().split()
+    dic_account[id] = password
+
+for j in range(M):
+    search_id = sys.stdin.readline().rstrip()
+    print(dic_account.get(search_id))
