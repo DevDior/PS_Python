0485911cfc1834ba5548fb5e04a86cf16859a2d2
Hyeokgi Lee <chinup1004@koreatech.ac.kr>
2022-12-27 15:50:58 +0900

    Clear

---
+++ BAEKJOON/11659.py
@@ -0,0 +1,19 @@
+# 구간 합 구하기 4
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+num_list = list(map(int, sys.stdin.readline().split()))
+sum_list = []
+sum_list.append(num_list[0])
+
+for i in range(1, N):
+    sum_list.append(sum_list[i-1] + num_list[i])
+
+for _ in range(M):
+    i, n = map(int, sys.stdin.readline().split())
+    if i == n:
+        print(num_list[i-1])
+    elif i == 1:
+        print(sum_list[n-1])
+    else:
+        print(sum_list[n-1] - sum_list[i-2])
