e4af279545ddab99f5fce5cc3ad4e45ddac120fc
Hyeokgi <71318080+JuniorHyeokgi@users.noreply.github.com>
2022-12-28 13:07:41 +0900

    DP Clear

---
+++ BAEKJOON/11726.py
@@ -0,0 +1,10 @@
+# 2xn 타일링
+
+import sys
+n = int(sys.stdin.readline())
+answer_list = [1, 2]
+
+for i in range(2, n):
+    answer_list.append(sum(answer_list[i-2:i]))
+
+print(answer_list[n-1]%10007)
