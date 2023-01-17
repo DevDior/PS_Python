61917eb582c4b129ad4a9ec3e626711c060a14c1
Hyeokgi <71318080+JuniorHyeokgi@users.noreply.github.com>
2023-01-17 13:47:06 +0900

    Clear

---
+++ 1074.py
@@ -0,0 +1,33 @@
+# Z
+import sys
+
+N, r, c = map(int, sys.stdin.readline().split())
+answer = 0
+
+def solution(x, y, n):
+    global answer
+    
+    if n == 0:
+        return
+
+    split_size = (2**n//2)**2
+    
+    # 1사분면
+    if x >= 2**n//2 and y < 2**n//2:
+        answer += split_size
+        solution(x-2**n//2, y, n-1)
+    # 2사분면
+    elif x < 2**n//2 and y < 2**n//2:
+        solution(x, y, n-1)
+    # 3사분면
+    elif x < 2 **n//2 and y >= 2**n//2:
+        answer += split_size * 2
+        solution(x, y-2**n//2, n-1)
+    # 4사분면
+    else:
+        answer += split_size * 3
+        solution(x-2**n//2, y-2**n//2, n-1)
+    
+    return answer
+
+print(solution(c, r, N))
