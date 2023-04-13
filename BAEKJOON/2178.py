830e6146db94b1066014d665459453017c8ba0e6
DevDior <chinup1004@naver.com>
2023-04-14 03:51:53 +0900

    Clear

---
+++ BAEKJOON/2178.py
@@ -0,0 +1,22 @@
+# 미로 탐색
+
+import sys
+from collections import deque
+
+N, M = map(int, sys.stdin.readline().split())
+matrix = [list(map(int, sys.stdin.readline().strip())) for _ in range(N)]
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+queue = deque()
+
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = dx[i] + x, dy[i] + y
+            if 0 <= nx < N and 0 <= ny < M and matrix[nx][ny] == 1:
+                matrix[nx][ny] = matrix[x][y] + 1
+                queue.append([nx, ny])
+
+queue.append([0, 0])
+bfs()
+print(matrix[N-1][M-1])
