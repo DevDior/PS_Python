85ee9c7b236c2721972bebeec3da3e7338b1f364
DevDior <chinup1004@naver.com>
2023-12-03 17:58:31 +0900

    Greedy, Clear

---
+++ BAEKJOON/1026.py
@@ -0,0 +1,17 @@
+# 보물, 그리디
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a_list = list(map(int, input().split()))
+b_list = list(map(int, input().split()))
+
+a_list.sort()
+b_list.sort(reverse=True)
+
+result = 0
+for i in range(n):
+  result += a_list[i] * b_list[i]
+
+print(result)
